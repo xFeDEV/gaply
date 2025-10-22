cf7d082a2079d8a5634adaad3f28546b746a565b
Daniel Santiago Caicedo Rodriguez <daniels_caicedo@soy.sena.edu.co>
2025-10-21 19:47:55 -0500

    Ip-Database

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
 # URL de conexión (reemplaza ${VPS_IP} por la IP real de tu servidor)
-DATABASE_URL = "postgresql+asyncpg://admin:supersecret123@<VPS_IP>:5432/gaply"
+DATABASE_URL = "postgresql+asyncpg://admin:supersecret123@20.57.33.149:5432/gaply"
 
 # Crea el motor asíncrono
 engine = create_async_engine(DATABASE_URL, echo=True)
